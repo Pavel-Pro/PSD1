a78b2145ed6ec06f94680b47ebb2801322e388e7
Pavel-Pro <proskpavel@gmail.com>
2021-04-09 17:27:38 +0300

    Адаптив 1400

--- js/main.js
+++ js/main.js
@@ -8,12 +8,35 @@ $(document).ready(function() {
 	$('.wrap__btn').on('click', function() {	
 		$(this).parent().toggleClass('unvisible');
 	});
-	$('.person__slider').slick({
+	$('.testimonials__items').slick({
 		dots: true,
- 		infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        draggable: false
+		arrows: false
+	});
+	$('.about-person-slider').slick({
+	    slidesToShow: 1,
+	    slidesToScroll: 1,
+	    arrows: false,
+	    asNavFor: '.our-team__items'
+
+	});
+	$('.our-team__items').slick({
+	    slidesToShow: 4,
+	    slidesToScroll: false,
+	    asNavFor: '.about-person-slider',
+	    dots: false,
+	    centerMode: true,
+	    focusOnSelect: true,
+	    arrows: false,
+	    centerPadding: '0px',
+	    responsive: [
+		    {
+		    	breakpoint: 1200,
+		    	settings: {
+		    		centerMode: true,
+		    		slidesToScroll: true,
+		    		slidesToShow: 3
+		    	}
+		    }
+	    ]
 	});
 });
