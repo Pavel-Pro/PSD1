782fdfd68f07f74b036c6e6fb4fcfd2cffa359a2
Pavel-Pro <proskpavel@gmail.com>
2021-04-18 12:29:07 +0300

    slidedown menu

--- js/main.js
+++ js/main.js
@@ -8,10 +8,6 @@ $(document).ready(function() {
 	$('.wrap__btn').on('click', function() {	
 		$(this).parent().toggleClass('unvisible');
 	});
-	$('.testimonials__items').slick({
-		dots: true,
-		arrows: false
-	});
 	$('.about-person-slider').slick({
 	    slidesToShow: 1,
 	    slidesToScroll: 1,
@@ -28,15 +24,85 @@ $(document).ready(function() {
 	    focusOnSelect: true,
 	    arrows: false,
 	    centerPadding: '0px',
-	    responsive: [
-		    {
+	    responsive: [{
 		    	breakpoint: 1200,
 		    	settings: {
 		    		centerMode: true,
 		    		slidesToScroll: true,
 		    		slidesToShow: 3
 		    	}
+		    },
+		    {
+		    	breakpoint: 950,
+		    	settings: {
+		    		centerMode: true,
+		    		slidesToScroll: true,
+		    		centerMode: true,
+		    		centerPadding: '50px',
+		    		slidesToShow: 2
+		    	}
+		    },
+		    {
+		    	breakpoint: 700,
+		    	settings: {
+		    		centerMode: true,
+		    		slidesToScroll: true,
+		    		centerMode: true,
+		    		centerPadding: '40px',
+		    		slidesToShow: 2
+		    	}
 		    }
 	    ]
+	});
+	$('.pricing__items').slick({
+		slidesToShow: 3,
+		responsive: [{
+			breakpoint: 700,
+			settings: {
+				centerMode: true,
+				slidesToScroll: true,
+				centerMode: true,
+				centerPadding: '100px',
+				focusOnSelect: true,
+				slidesToShow: 1
+			}
+		}]
+	});
+	$('.testimonials__items').slick({
+		dots: true,
+		arrows: false
+	});
+	$(window).on('scroll', function(){
+		var screenHeight = window.innerHeight;
+		var currentHeight = $(document).scrollTop();
+		if (currentHeight >= screenHeight) {
+			$('.to-top').fadeIn(1000);
+		} else {
+			$('.to-top').fadeOut(500);
+		}
+	});
+
+	$('.to-top').on('click', function() {
+		$(document).scrollTop(0);
+	});
+
+	$('.to-top').on('mouseover', function() {
+  		$(this).addClass('hover');
+	});
+
+	$('.to-top').on('mouseleave', function() {
+  		$(this).removeClass('hover');
+	});
+
+	$(window).on('scroll', function(){
+		var menuShow = $('.aboutus').offset().top;
+		var currentHeight = $(document).scrollTop();
+		if (currentHeight >= menuShow) {
+			$('.main').addClass('fixed');
+			$('.header').addClass('fixed');
+		} else {
+			$('.main').removeClass('fixed');
+			$('.header').removeClass('fixed');
+		}
 	});
 });
